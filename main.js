f7c25ed1afdf493014902fc2154f918e0641412b
Jaweria Rashdi <rashdijaweria@gmail.com>
2024-04-17 14:16:52 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+#! /use/bin/env mode
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+let todos = [];
+let condition = true;
+while (condition) {
+    const todosQuestions = await inquirer.prompt([
+        {
+            name: 'task',
+            type: 'input',
+            message: chalk.blue('What Would You Like To Add In Your Todos?')
+        },
+        {
+            name: 'addMore',
+            type: 'confirm',
+            message: chalk.green('Would You Like To Add More Todos?'),
+            default: true
+        }
+    ]);
+    todos.push(todosQuestions.task);
+    console.log(chalk.yellow('Current Todos:'), todos);
+    condition = todosQuestions.addMore;
+}
